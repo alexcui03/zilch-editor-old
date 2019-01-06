1008368a1458c6e142c7305fd2c1f7aa46cf0988
Alex Cui <cuizhihui030925@outlook.com>
2019-01-06 20:21:45 +0800

    0.2.0.435

--- src/widget/SpriteItem.cpp
+++ src/widget/SpriteItem.cpp
@@ -5,8 +5,12 @@
 SpriteItem::SpriteItem(ScratchSprite *Sprite, QWidget *parent) : Widget(parent) {
 	this->Sprite = Sprite;
 	this->setObjectName("SpriteItem");
+	this->move(240, 180);
 
 	this->Label = new QLabel(this);
 	this->Label->move(0, 0);
 	this->Label->setPixmap(QPixmap::fromImage(*static_cast<QImage*>(this->Sprite->CostumeList[this->Sprite->CurrentCostume].Resourse->Data)));
+	this->Label->adjustSize();
+
+	this->adjustSize();
 }
