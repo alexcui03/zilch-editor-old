e733f850ec9bbebc4a1f38d54f3615fb96137a6c
Alex Cui <cuizhihui030925@outlook.com>
2019-02-26 21:20:51 +0800

    0.2.2.608B

--- src/widget/costumepart/CostumeView.h
+++ src/widget/costumepart/CostumeView.h
@@ -6,12 +6,6 @@
 
 #include "../Widget.h"
 
-#define A "SS"
-#define B "0.0.0"
-#define C #A##"AAA"
-
-constexpr const char *c = C;
-
 class CostumeView : public Widget {
 public:
 	CostumeView(QWidget *parent = nullptr);
